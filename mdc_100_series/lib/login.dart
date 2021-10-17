5df9f0cd735c51f620c8dc3b0553670b4c32b998
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2021-10-17 10:35:39 +0200

    Nomiso changed it

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -23,6 +23,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +45,50 @@ class _LoginPageState extends State<LoginPage> {
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
             // TODO: Add TextField widgets (101)
+            // [Name]
+            TextField(
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+              controller: _usernameController,
+            ),
+            // spacer
+            const SizedBox(height: 12.0),
+            // [Password]
+            TextField(
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+              controller: _passwordController,
+            ),
+
             // TODO: Add button bar (101)
+            ButtonBar(
+              // TODO: Add a beveled rectangular border to CANCEL (103)
+              children: <Widget>[
+                // TODO: Add buttons (101)
+            TextButton(
+              child: const Text('CANCEL'),
+              onPressed: () {
+                // TODO: Clear the text fields (101)
+                _usernameController.clear();
+                _passwordController.clear();
+              },
+            ),
+            // TODO: Add an elevation to NEXT (103)
+            // TODO: Add a beveled rectangular border to NEXT (103)
+            ElevatedButton(
+              child: const Text('NEXT'),
+              onPressed: () {
+            // TODO: Show the next page (101) 
+             Navigator.pop(context);
+              },
+            ),
+              ],
+            ),
           ],
         ),
       ),
